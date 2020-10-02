6d97b17f4d7c6886268152d9f2799b4c0a959fe6
Nullish <richerde43@gmail.com>
2020-10-02 13:18:18 +0100

    Colour format console logs

--- bigQueryInsert.js
+++ bigQueryInsert.js
@@ -1,5 +1,6 @@
 // Import the Google Cloud client library
 const {BigQuery} = require('@google-cloud/bigquery');
+const colors = require('colors'); // Colours for console output
 
 const insertRowsAsStream = async(datasetId = "lighthouse", tableId = "tbTmpLH", rows) => {
   try {
@@ -12,7 +13,8 @@ const insertRowsAsStream = async(datasetId = "lighthouse", tableId = "tbTmpLH", 
   .dataset(datasetId)
   .table(tableId)
   .insert(rows);
-  console.log(`BigQuery record inserted to: \ndataset ${datasetId}, table: \n${tableId}\nRecords:${rows}`);
+  let conslog = `INFO: `.green +  `BigQuery stream record. ` + `Dataset: `.green + `${datasetId} ` + `Table: `.green + `${tableId}`;
+  console.log(conslog);
 } catch(e) {
     // statements
     console.log(e.errors);
