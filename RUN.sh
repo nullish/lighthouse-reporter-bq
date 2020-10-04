aab92295ac339f20dcf65bdecb8a5ae6db0cc20a
Nullish <richerde43@gmail.com>
2020-10-05 00:34:56 +0100

    Add GDS URL to completion statement.

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,7 @@
 # Initiate environment variable file for Google service account authentication and run test module.
 source ./.env
 npm start
+echo
+echo "Lighthouse reporting has completed."
+echo
+echo "View results at: https://datastudio.google.com/reporting/e6cb7b35-83d2-418a-9822-12166dab10f6/page/eoWiB"
