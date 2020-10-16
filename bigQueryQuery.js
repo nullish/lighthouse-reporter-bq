c1d772333a4081888e0894e9321286d3e7ba74ae
Nullish <richerde43@gmail.com>
2020-10-16 20:06:34 +0100

    Initial query of raw_reports

---
+++ bigQueryQuery.js
@@ -0,0 +1,54 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// REPO: https://raw.githubusercontent.com/googleapis/nodejs-bigquery/master/samples/query.js
+
+'use strict';
+
+function main() {
+  // [START bigquery_query]
+  // [START bigquery_client_default_credentials]
+  // Import the Google Cloud client library using default credentials
+  const {BigQuery} = require('@google-cloud/bigquery');
+  const bigquery = new BigQuery();
+  // [END bigquery_client_default_credentials]
+  async function query() {
+    // Queries the U.S. given names dataset for the state of Texas.
+
+    const query = `SELECT report
+FROM \`speed-test-286619.lighthouse.raw_reports\`
+WHERE fetch_time = '2020-10-16 15:47:54.122 UTC'`;
+
+    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+    const options = {
+      query: query,
+      // Location must match that of the dataset(s) referenced in the query.
+      location: 'US',
+    };
+
+    // Run the query as a job
+    const [job] = await bigquery.createQueryJob(options);
+    console.log(`Job ${job.id} started.`);
+
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults();
+
+    // Print the results
+    console.log('Rows:');
+    rows.forEach(row => console.log(row));
+  }
+  // [END bigquery_query]
+  query();
+}
+main(...process.argv.slice(2));
